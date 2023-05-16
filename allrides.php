253da73052895022ede3a49087e17affbe7e3251
Mathewsmca <98259029+Mathewsmca@users.noreply.github.com>
2023-05-16 18:52:34 +0530

    Update allrides.php

--- allrides.php
+++ allrides.php
@@ -71,10 +71,10 @@ if(isset($_GET['action']))
   <label for="filter">FILTER BY CAB</label>
   <select name="cfil" id="cfil">
   <option value="" selected>NONE</option>
-  <option value="CedMini">CedMini</option>
-  <option value="CedMicro">CedMicro</option>
-  <option value="CedRoyal">CedRoyal</option>
-  <option value="CedSUV">CedSUV</option>
+  <option value="Mini">Mini</option>
+  <option value="Micro">Micro</option>
+  <option value="Royal">Royal</option>
+  <option value="SUV">SUV</option>
   </select>
   </div>
   </div>
