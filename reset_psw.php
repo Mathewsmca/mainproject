4d47452bca2568c706813c3817b2a1776129fa77
Mathewsmca <mathewsebastian2023b@mca.ajce.in>
2023-05-31 20:29:29 +0530

    paswd

--- reset_psw.php
+++ reset_psw.php
@@ -34,7 +34,7 @@ if (isset($_POST['submit'])) {
           $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
           $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
           $mail->Username   = 'cabbooking05@gmail.com';                      //SMTP username
-          $mail->Password   = 'khmypqqamoduenue';                               //SMTP password
+          $mail->Password   = 'khmypqqamoduenuexx';                               //SMTP password
           $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
           $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
   
